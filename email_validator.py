f73c568f6e915d8006dd6798ff16f46762d99ca9
jeevithan003 <jeevijeevithan24@gamil.com>
2025-08-04 21:41:56 +0530

    new featurs are added

--- email_validator.py
+++ email_validator.py
@@ -1,8 +1,10 @@
 c=input("enter the email: ")
-
-
+l=len(c)
+if l==0:
+    print("email cannot be empty")
 
 if "@" in c and "." in c:
     print("its a  valid emial ")
 else:
     print("its is invlaid email")
+    
